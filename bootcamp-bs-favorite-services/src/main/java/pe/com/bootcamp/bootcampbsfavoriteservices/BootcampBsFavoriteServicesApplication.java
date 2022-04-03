472728fad59346ccf5d48557b7474fbe23035257
Dyago Diaz <dyago.diaz@gmail.com>
2022-04-02 22:45:45 -0500

    Initial commit bootcamp-bs-favorite-services

---
+++ bootcamp-bs-favorite-services/src/main/java/pe/com/bootcamp/bootcampbsfavoriteservices/BootcampBsFavoriteServicesApplication.java
@@ -0,0 +1,13 @@
+package pe.com.bootcamp.bootcampbsfavoriteservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampBsFavoriteServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampBsFavoriteServicesApplication.class, args);
+	}
+
+}
---
+++ bootcamp-bs-favorite-services/src/test/java/pe/com/bootcamp/bootcampbsfavoriteservices/BootcampBsFavoriteServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.bootcamp.bootcampbsfavoriteservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampBsFavoriteServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
