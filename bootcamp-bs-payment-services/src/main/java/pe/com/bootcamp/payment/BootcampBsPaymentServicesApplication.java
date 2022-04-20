78b716a2bdbb5495654f013eea4b28222e37213a
Dyago Diaz <dyago.diaz@gmail.com>
2022-04-19 22:06:08 -0500

    initial commit bootcamp-bs-payment-services

---
+++ bootcamp-bs-payment-services/src/main/java/pe/com/bootcamp/payment/BootcampBsPaymentServicesApplication.java
@@ -0,0 +1,13 @@
+package pe.com.bootcamp.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampBsPaymentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampBsPaymentServicesApplication.class, args);
+	}
+
+}
