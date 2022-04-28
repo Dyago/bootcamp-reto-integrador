302a87a9a51c71224e5406c7433edb62386414d5
Dyago Diaz <dyago.diaz@gmail.com>
2022-04-27 22:55:28 -0500

    initial commit bootcamp-ux-banca-movil

---
+++ bootcamp-ux-banca-movil/src/main/java/pe/com/bootcamp/channel/BootcampUxBancaMovilApplication.java
@@ -0,0 +1,13 @@
+package pe.com.bootcamp.channel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampUxBancaMovilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampUxBancaMovilApplication.class, args);
+	}
+
+}
---
+++ bootcamp-ux-banca-movil/src/test/java/pe/com/bootcamp/channel/BootcampUxBancaMovilApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.bootcamp.channel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampUxBancaMovilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
